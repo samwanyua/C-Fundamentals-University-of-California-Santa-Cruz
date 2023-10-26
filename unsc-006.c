73cc9782c1af07b0b6c514204ba7c688e92c17e5
samwanyua <samexample8@gmail.com>
2023-10-26 16:09:50 +0300

    Keywords in C

---
+++ unsc-006.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{   
+    /* The following are keywords in C:
+    auto (absolute)	break	case	char	
+    const	continue	default	do
+    double	else	enum	extern	float	
+    for	goto (absolute)	if
+    int	long	register(absolute)	return	
+    short	signed	sizeof	static (absolute)
+    struct	switch	typedef	union	unsigned	
+    void	volatile	while
+    */
+    printf(" Hello world\n");
+    return 0;
+}    
