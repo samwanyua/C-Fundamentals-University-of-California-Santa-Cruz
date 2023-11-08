b83932a2250194dc3a1ff8936ce8b00e17480bf8
samwanyua <samexample8@gmail.com>
2023-11-08 12:54:52 +0300

    Break and Continue and Switch

---
+++ unsc-024.c
@@ -0,0 +1,20 @@
+/* Break and Continue and Switch*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    /*
+   So in a switch statement, basically you switched 
+   on an integral expression. 
+
+   switch (i) {
+   case 1: a = 2*i; break;
+   case 2: a = 3 *i; break;
+   default: a = 4 * i; 
+   } 
+    
+    
+    */
+    return0;
+}
