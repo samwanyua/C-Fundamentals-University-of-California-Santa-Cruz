4d690a94b5193bb82cfacf48748b68d6f7b3eb2f
samwanyua <samexample8@gmail.com>
2023-11-13 17:03:14 +0300

    Function definition

---
+++ unsc-032.c
@@ -0,0 +1,36 @@
+/*Function definition*/
+#include <stdio.h>
+
+int main (void)
+{
+/* 
+Functions - paragraph of coding by DR. P
+int main(void) is a fuction
+also int printf()
+
+Why are functions important? Crucial for reuse for code or libraries ex. stdio.h
+>>Fundamental for encapsulation - A function is a form of encapsulation. It allows you to structure a big piece of code.
+>>A function largely should be able to do one thing well. Think of rand, think of printf, think of square root. 
+Syntax:
+    type funtion_name (parameter list)  //type ie int, float
+        {
+            // Body of the function
+
+          declaration 
+          executable statements  
+
+        example
+        void wrt_address(void)
+        {
+            printf("clause\n");
+            printf("North Pole\n");
+
+        }
+
+        
+      
+
+
+*/
+    return 0;
+}
