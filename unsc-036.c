1740320cf73674ccc8153143eb6630149273c92f
samwanyua <samexample8@gmail.com>
2023-11-13 18:07:11 +0300

    Function prototype

---
+++ unsc-036.c
@@ -0,0 +1,26 @@
+/*Function Prototype*/
+
+#include <stdio.h>
+
+int main (void)
+{
+ /* 
+ DECLARATIONS & PROTOTYPES
+ Declaring soething first before it is defined is also called a prototype
+ for example
+
+ double cube (double);
+ int main()
+ {
+    a = cube(3.5);
+ }
+
+This definition of prototype can be just with parameter type or with names
+double cube (double x); is equivalent to double cube(double); //signature
+
+special signature for example 
+int printf(...);
+
+ */
+    return 0;
+}
