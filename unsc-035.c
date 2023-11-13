5ea01b0e86b1d009ce08cb77b126563dc2dc54b3
samwanyua <samexample8@gmail.com>
2023-11-13 17:42:25 +0300

    Return statements

---
+++ unsc-035.c
@@ -0,0 +1,23 @@
+/*Return statements*/
+#include <stdio.h>
+
+int main (void)
+{
+    /*
+    return 0; meaning the program runs in an expected way
+    syntax: return; or return expression;
+    for example:
+        return;
+        return 0; or return (0);
+        return (a+b);
+    Simple use in a function
+    
+    double cube (double x)
+    {
+        return (x * x * x);
+    }
+    
+    */
+    
+    return 0;
+}
