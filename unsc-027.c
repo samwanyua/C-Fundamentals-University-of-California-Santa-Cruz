817767a44f03a361f3658780b8217c074f1b634c
samwanyua <samexample8@gmail.com>
2023-11-13 16:01:07 +0300

    Break and continue and switch

---
+++ unsc-027.c
@@ -0,0 +1,18 @@
+/*Break and continue switch file*/
+#include <stdio.h>
+
+int main (void)
+{
+/* 
+switch(i)
+{
+    case 1: a = 2* i; break;
+    case 2: a = 3 * i; break;
+    default: a = 4 * i;
+}
+
+
+
+*/
+    return 0;
+}
