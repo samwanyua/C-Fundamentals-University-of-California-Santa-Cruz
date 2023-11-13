f8308ff32cc7a5d71d0ad866beaa2396adcd3b5a
samwanyua <samexample8@gmail.com>
2023-11-13 23:42:28 +0300

    Function definition and scope rules

---
+++ unsc-039.c
@@ -0,0 +1,16 @@
+/*Function definition and scope rules*/
+
+#include <stdio.h>
+int main (void)
+{
+/*
+SCOPE RULES
+Involves declarations - declarations typically occur at the head of blocks, and blocks can exist in different architectures. There can be nested blocks and 
+there can be parallel blocks.
+
+
+
+
+*/
+    return 0;
+}
