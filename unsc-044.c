22506247102fb1d02dd1ddbcc3a9cbb09ae41fd5
samwanyua <samexample8@gmail.com>
2023-11-15 14:27:00 +0300

    Pointers and simple arrays

---
+++ unsc-044.c
@@ -0,0 +1,33 @@
+/*Pointers and Simple arrays*/
+#include <stdio.h>
+ 
+int main(void)
+{
+/*
+Aggregate data
+*simple 1-D arrays
+int data [100];
+int = type, data = ID, 100 = Size
+
+we get:
+data[0]
+data[1]
+.
+.
+.
+data[99]
+
+Why is this useful: to store, retrieve and process large data sets
+
+
+
+
+
+
+
+*/
+
+
+
+    return 0;
+}
