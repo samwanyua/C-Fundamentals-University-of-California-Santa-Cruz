e56f75d82e3cabca299c260aa6a852befc284637
samwanyua <samexample8@gmail.com>
2023-11-15 17:07:22 +0300

    Pointers Intro

---
+++ unsc-047.c
@@ -0,0 +1,23 @@
+/*Pointers*/
+
+#include <stdio.h>  
+
+int main(void)
+{
+/*
+Pointers
+int a; means you create a variable in memory which has 4 bytes
+
+a = 3;  what that means is in these four bytes we put inappropriate representation binary three and it's going to be in some location in memory
+
+Address calculation;
+Address = Base address + 2 * length of 4 byte int
+
+pointer variables
+int*P = &a;
+int = underlying type, & = address of a
+
+
+*/
+    return 0;
+}
