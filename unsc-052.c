6e0e4778f3657bc029bf0e78242c5e4e94765613
samwanyua <samexample8@gmail.com>
2023-11-15 19:47:59 +0300

    Merge sort overview

---
+++ unsc-052.c
@@ -0,0 +1,38 @@
+/*Merge sort overview*/
+#include <stdio.h>
+
+int main(void)
+{
+/*
+Merge sort
+the heart of merge sort is take small piles,
+ merge them, take bigger piles, merge them. 
+ Keep going until everything is merged into one sorted order
+
+void merge (int a[], int b[], int c[], int m, int n)
+{
+    int i = 0, j = 0, k = 0;
+    while(i < m && j < n)
+        if (a[i] < b[j])
+        {
+            C[k++] = a [i++];
+        }
+            
+        else
+        {
+            c[k++] = b[j++];
+        }
+
+        while (i < m)
+            c[k++] = b[j++];
+
+        while (j < n)
+            c[k++] = b[j++];
+}
+
+
+*/
+
+
+    return 0;
+}
