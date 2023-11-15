9aa5bb89c37bae972c7950e7bd0b444db97f9512
samwanyua <samexample8@gmail.com>
2023-11-15 14:37:41 +0300

    Initialize arrays intro

---
+++ unsc-045.c
@@ -0,0 +1,28 @@
+/*Intialize Arrays*/
+#include <stdio.h>
+
+int main(void)
+{
+/*
+Arrays initialization
+string is an array of char
+
+int ssn = 55511122;
+int data[5]; 
+int data[5] = {1,2,3,4,5};
+int data[5] = {0};
+int data[] = {1,2,3};
+
+char str[] = "a b c";
+
+
+'\0' is a sentinel - a way to find the end of a string
+
+
+
+
+
+
+*/
+    return 0;
+}
